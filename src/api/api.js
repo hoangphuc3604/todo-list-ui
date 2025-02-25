ba2ef72df3578077f6433ac4f595fe52fcc28831
22120278 <nvhphuc364@gmail.com>
2025-02-25 18:30:08 +0700

    Add API utility functions for task management

---
+++ src/api/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+console.log(process.env.REACT_APP_API_SERVER);
+
+export const getTasks = async () => {
+  const response = await axios.get(process.env.REACT_APP_API_SERVER + "/tasks");
+  return response.data;
+};
+
+export const addTask = async (task) => {
+  const response = await axios.post(
+    process.env.REACT_APP_API_SERVER + "/tasks",
+    {
+      title: task.title,
+      description: task.description,
+    }
+  );
+  return response.data;
+};
+
+export const toggleTask = async (id, completed) => {
+  await axios.put(`${process.env.REACT_APP_API_SERVER + "/tasks"}/${id}`, {
+    completed: !completed,
+  });
+};
+
+export const deleteTask = async (id) => {
+  await axios.delete(`${process.env.REACT_APP_API_SERVER + "/tasks"}/${id}`);
+};
